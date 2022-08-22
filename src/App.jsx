f1b590ab9c0f6a0421bbe82b75a3dbd8a8e19678
ProjectISAEL <projectisael@gmail.com>
2022-08-21 23:33:46 -0500

    Arreglo N

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ import EditarCliente from './pages/EditarCliente'
 import VerCliente from './pages/VerCliente'
 
 function App() {
- 
+
   return (
     <div>
         <BrowserRouter>
           <Routes>
+
+          <Route path='/'
+            element={
+              <Navigate to="/clientes" replace={true} />
+            }
+          />
             <Route path='/clientes' element={<Layout />}>
                 <Route index element={<Inicio />} />
                 <Route path='nuevo' element={<NuevoCliente />} />
